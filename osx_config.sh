fcd37c07e7074d7b80c421b2e17eae0e5ba7c7d5
Alex Louden <alex@louden.com>
2014-12-08 19:33:25 +0800

    Update osx_config.sh

--- osx_config.sh
+++ osx_config.sh
@@ -1,3 +1,4 @@
+sudo -v
 
 echo "Expanding the save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -16,6 +17,60 @@ echo "Ask for password immediately after sleep/screensaver"
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-echo "Showing battery percentage"
+# Menu bar: show battery percentage
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
+echo "Disabling warning on change file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo "Setting default finder view to column"
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+echo "Disabling the Time Machine new disk requests dialog"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Don't automatically switch to a Space with open windows for and application when switching to it 
+defaults write com.apple.dock workspaces-auto-swoosh -boolean NO
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Trackpad: swipe between pages with three fingers
+defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerHorizSwipeGesture -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 1
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+############################################################
+# Transmission settings
+mkdir -p ~/Downloads/Incomplete
+defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
+
+echo "Don't prompt for confirmation before downloading"
+defaults write org.m0k.transmission DownloadAsk -bool false
+
+echo "Trash original torrent files"
+defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
+echo "Hide the donate message"
+defaults write org.m0k.transmission WarningDonate -bool false
+
+echo "Hide the legal disclaimer"
+defaults write org.m0k.transmission WarningLegal -bool false
+
+############################################################
+# Sublime Text
+# Use as git editor
+git config --global core.editor "subl -n -w"

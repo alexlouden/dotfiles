f562041a0c2531baaad9d17fb77299be7ef56bfe
Alex Louden <alex@louden.com>
2014-12-10 15:38:16 +0800

    Homebrew and other basics setup

---
+++ brew.sh
@@ -0,0 +1,38 @@
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+# Now install standard things
+binaries=(
+  python
+  node
+  tree
+  git
+)
+
+echo "installing binaries..."
+brew install ${binaries[@]}
+
+echo "installing oh-my-zsh..."
+curl -L http://install.ohmyz.sh | sh

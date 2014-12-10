02a563afb639146dd89f210779a9ab1975f91b86
Alex Louden <alex@louden.com>
2014-12-10 12:50:30 +0800

    echo to comment

--- osx_config.sh
+++ osx_config.sh
@@ -1,32 +1,32 @@
 sudo -v
 
-echo "Expanding the save panel by default"
+# Expanding the save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-echo "Hiding spotlight search icon"
+# Hiding spotlight search icon
 sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
 
-echo "Automatically quit printer app once the print jobs complete"
+# Automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-echo "Disabling smart dashes and quotes"
+# Disabling smart dashes and quotes
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-echo "Ask for password immediately after sleep/screensaver"
+# Ask for password immediately after sleep/screensaver
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Menu bar: show battery percentage
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
-echo "Disabling warning on change file extension"
+# Disabling warning on change file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-echo "Setting default finder view to column"
+# Setting default finder view to column
 defaults write com.apple.finder FXPreferredViewStyle Clmv
 
-echo "Disabling the Time Machine new disk requests dialog"
+# Disabling the Time Machine new disk requests dialog
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Don't automatically switch to a Space with open windows for and application when switching to it 
@@ -58,16 +58,16 @@ mkdir -p ~/Downloads/Incomplete
 defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
 defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
 
-echo "Don't prompt for confirmation before downloading"
+# Don't prompt for confirmation before downloading
 defaults write org.m0k.transmission DownloadAsk -bool false
 
-echo "Trash original torrent files"
+# Trash original torrent files
 defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
 
-echo "Hide the donate message"
+# Hide the donate message
 defaults write org.m0k.transmission WarningDonate -bool false
 
-echo "Hide the legal disclaimer"
+# Hide the legal disclaimer
 defaults write org.m0k.transmission WarningLegal -bool false
 
 ############################################################
